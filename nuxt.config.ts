a30d6318d57398704dea7f510849d51dfb7bfc9c
Elgars <elgars.upitis@gmail.com>
2025-06-27 14:52:02 +0300

    GitHub Pages & Actions configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
   },
   ssr: false,
   app: {
-    baseURL: process.env.NODE_ENV || '/',
+    baseURL: process.env.NUXT_APP_BASE_URL || '/',
     buildAssetsDir: 'assets'
   },
   nitro: {
