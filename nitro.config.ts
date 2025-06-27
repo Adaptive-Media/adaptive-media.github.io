f06a032d21938623d3afd0d68281750b8067117f
Elgars <elgars.upitis@gmail.com>
2025-06-27 16:29:26 +0300

    GitHub Pages & Actions configuration

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,12 +1,12 @@
-export default defineNitroConfig({
-    prerender: {
-        routes: [
-            '/api/product-categories',
-            '/api/popular-products',
-            '/api/pagination-data',
-            '/api/product-categories?limit=8&offset=0',
-            '/api/popular-products?categoryId&limit=6&offset=0&sortBy=popular&filter',
-            '/api/pagination-data?type=products'
-        ]
-    }
-});
+// export default defineNitroConfig({
+//     prerender: {
+//         routes: [
+//             '/api/product-categories',
+//             '/api/popular-products',
+//             '/api/pagination-data',
+//             '/api/product-categories?limit=8&offset=0',
+//             '/api/popular-products?categoryId&limit=6&offset=0&sortBy=popular&filter',
+//             '/api/pagination-data?type=products'
+//         ]
+//     }
+// });
